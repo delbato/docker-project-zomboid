31743de0f6665f64a5b2bcde79fc50136e03e4a0
Daniel Wanner <daniel.wanner@pexon-consulting.de>
2022-11-21 14:53:02 +0100

    Modifying server installation

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,14 +1,22 @@
 #! /bin/bash
 
+echo "Chosen username and password: $STEAM_USERNAME:$STEAM_PASSWORD"
+env
+
 check_if_installed () {
     echo false
 }
 
 install_server () {
-    echo "Chosen username and password: $STEAM_USERNAME:$STEAM_PASSWORD"
+    if [ -z "${STEAM_USERNAME}" ] && [ -z $STEAM_PASSWORD ]; then
+        echo "Installing anonymously."
+        steamcmd +force_install_dir /server +login anonymous +app_update 380870 validate +quit
+    else
+        echo "Installing with steam account.";
+        steamcmd +force_install_dir /server +login $STEAM_USERNAME $STEAM_PASSWORD +app_update 380870 validate +quit
+    fi
     echo "I am: $(whoami)"
     echo "My home directory is: $HOME"
-    steamcmd +force_install_dir /server +login anonymous +app_update 380870 validate +quit
 }
 
 installed=$(check_if_installed)
